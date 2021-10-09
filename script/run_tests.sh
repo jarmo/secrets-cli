9f1a80d427a6a5f7b5bf4780be54e9425826569a
Jarmo Pertman <jarmo@jarmopertman.com>
2021-10-09 21:07:05 +0300

    Fix shebangs

--- script/run_tests.sh
+++ script/run_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -o pipefail
 go test -mod=vendor -v ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
